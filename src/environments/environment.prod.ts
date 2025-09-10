505ed3c3c41dd84294fdf6d73ff34a5b4b5e8ea0
RealInfoProduct <reallocinfotechproduct@gmail.com>
2025-09-10 11:18:04 +0530

    simple changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,6 +9,6 @@ export const environment = {
         storageBucket: "myinvoice-e4e41.appspot.com",
         messagingSenderId: "364972721626",
         appId: "1:364972721626:web:2647fef078ef3581ec6ce3",
-        measurementId: "G-143NM6E0N7"
+        measurementId: "G-143NM6E0N7" 
       }
 }

602c1f7a8b51d2906cdd7b45264d733a16b68a9d
RealInfoProduct <reallocinfotechproduct@gmail.com>
2026-04-01 16:28:37 +0530

    apply accountYear on the invoiceList

--- src/app/pages/master/invoice-list/add-invoice/add-invoice.component.ts
+++ src/app/pages/master/invoice-list/add-invoice/add-invoice.component.ts
@@ -359,7 +359,7 @@ export interface InvoiceData {
       if (res) {
         this.invoiceList = res.filter((id:any) => 
           id.userId === localStorage.getItem("userId") && 
-          // id.accountYear === localStorage.getItem("accountYear") &&
+          id.accountYear === localStorage.getItem("accountYear") &&
           id.firmId === firmId
          )     
         this.maxInvoiceNumber = this.invoiceList.length > 0 
